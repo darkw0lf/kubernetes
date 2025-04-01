2646cb71324c468adef21dc384955edae706550f
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2025-04-01 14:47:41 +0200

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -63,14 +63,3 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 echo 'export KUBECONFIG=$HOME/.kube/config' >> $HOME/.bashrc
 source ~/.bashrc
-
-kubectl get nodes -o wide
-kubectl get pods -A -o wide
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-kubectl get pods -n kube-system -o wide
-
-#sudo kubeadm token create --print-join-command
-
-kubectl get nodes -o wide
-kubectl run my-pod --image=nginx --port=80
-kubectl get pods -o wide
